bd4997253fe0006427ba079bd62c24294e60c7b8
Rui Abreu Ferreira <raf-ep@gmx.com>
2017-05-04 13:26:24 +0100

    Fix firefox arguments for Windows

--- ff/src/runner.rs
+++ ff/src/runner.rs
@@ -2,9 +2,9 @@ use std::convert::From;
 use std::io::ErrorKind;
 use std::io::Error as IoError;
 use std::io::Result as IoResult;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::process;
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, Child};
 
 extern crate marionette;
 use mktemp::Temp;
@@ -13,6 +13,28 @@ use mozrunner::firefox_default_path;
 use mozprofile::profile::Profile;
 use mozprofile::preferences::Pref;
 use mozprofile::prefdata::FIREFOX_PREFERENCES;
+
+#[cfg(unix)]
+fn spawn_firefox(firefox_bin: &Path, profile: &Path) -> IoResult<Child> {
+    Command::new(firefox_bin)
+        .arg("--marionette")
+        .arg("--profile")
+        .arg(profile)
+        .stdout(Stdio::null()).stderr(Stdio::null())
+        .env("MOZ_NO_REMOTE", "1").env("NO_EM_RESTART", "1")
+        .spawn()
+}
+
+#[cfg(windows)]
+fn spawn_firefox(firefox_bin: &Path, profile: &Path) -> IoResult<Child> {
+    Command::new(firefox_bin)
+        .arg("-marionette")
+        .arg("-profile")
+        .arg(profile)
+        .stdout(Stdio::null()).stderr(Stdio::null())
+        .env("MOZ_NO_REMOTE", "1").env("NO_EM_RESTART", "1")
+        .spawn()
+}
 
 pub struct FirefoxRunner {
     pub process: process::Child,
@@ -49,13 +71,7 @@ impl FirefoxRunner {
         }
 
         let bin = firefox_default_path().unwrap_or(PathBuf::from("firefox"));
-        let child = Command::new(bin)
-            .arg("--marionette")
-            .arg("--profile")
-            .arg(&profile.path)
-            .stdout(Stdio::null()).stderr(Stdio::null())
-            .env("MOZ_NO_REMOTE", "1").env("NO_EM_RESTART", "1")
-            .spawn()?;
+        let child = spawn_firefox(&bin, &profile.path)?;
 
         info!("Started firefox on port {}", port);
 
