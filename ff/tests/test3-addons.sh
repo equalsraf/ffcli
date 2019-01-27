29ad32470e3f01077d72288178753e0b6d3e4f51
Rui Abreu Ferreira <raf-ep@gmx.com>
2019-01-27 20:19:26 +0000

    Fix addon test urls

--- ff/tests/test3-addons.sh
+++ ff/tests/test3-addons.sh
@@ -3,9 +3,9 @@
 . common.sh
 
 ADDON_UBLOCK=$(mktemp --suffix=.xpi)
-wget -O $ADDON_UBLOCK "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi"
+wget -O $ADDON_UBLOCK "https://addons.mozilla.org/firefox/downloads/file/1580486/ublock_origin-1.18.2-an+fx.xpi"
 ADDON_COOKIES=$(mktemp --suffix=.xpi)
-wget -O $ADDON_COOKIES "https://addons.mozilla.org/firefox/downloads/latest/self-destructing-cookies/addon-415846-latest.xpi"
+wget -O $ADDON_COOKIES "https://addons.mozilla.org/firefox/downloads/file/832803/self_destructing_cookies-0.1.0-an+fx.xpi"
 
 export FF_PORT=$(ff start)
 ff install $ADDON_UBLOCK
