32381aac42957a640953e182cf72aeb20f8ab976
equalsraf <undisclosed>
2023-07-27 15:10:28 +0100

    Fix tests

--- ff/tests/userprefs.rs
+++ ff/tests/userprefs.rs
@@ -7,17 +7,18 @@ use std::time::Duration;
 
 #[test]
 fn user_js_file() {
-    let browser = ff::Browser::start(65333,
+    let browser = ff::Browser::start(Some(65333),
                                          None,
                                          None,
                                          Some("tests/data/test-user.js"),
                                          None,
                                          None).unwrap();
 
-    ff::check_connection(browser.runner.port()).unwrap();
+    let port = browser.runner.port().unwrap();
+    ff::check_connection(port).unwrap();
 
     thread::sleep(Duration::new(5, 0));
-    let mut conn = MarionetteConnection::connect(browser.runner.port()).unwrap();
+    let mut conn = MarionetteConnection::connect(port).unwrap();
     let res = conn.get_pref("ff.testpref.canary").unwrap();
     assert_eq!(res, JsonValue::String("the canary is dead".to_string()));
     conn.quit().unwrap();
